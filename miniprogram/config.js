be9eff774e2fce6a89fcd38dbfe853255842ff7d
ParatrooperAndy <1468492471@qq.com>
2022-04-02 11:23:35 +0800

    Initial Commit

---
+++ miniprogram/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "appName":"POI地图",
+  "envID":"cloud1-3gbx9sw41cb2a321",
+  "mapSubKey":"FXZBZ-EFR3R-XQSWN-WSSEN-SEIPE-TNFVZ",
+  "center_longitude": 113.921736,
+  "center_latitude": 22.538017,
+  "dynamic_title":true,
+  "show_admin":false,
+  "default_scale":16
+}
